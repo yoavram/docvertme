6ae9e2c13a68d689c1debc6c6100f04a17d46fda
Yoav Ram <yoavram+github@gmail.com>
2013-01-10 00:12:14 +0200

    share button

--- static/docvertme.js
+++ static/docvertme.js
@@ -1,5 +1,5 @@
 function convert(from, to, content) {
-	var encoded = encodeURIComponent(content);
+	var encoded = url_encode(content);
 	var url = '/convert/' + from + '/' + to + '/' + encoded;
 	window.open(url, '_newtab');
 }
@@ -7,6 +7,21 @@ function convert(from, to, content) {
 function convert_btn_click() {
 	var to = $(this).attr('id').substring(3).toLowerCase();
 	var from = 'markdown';
-	var content = $('#editor-input').val();
+	var content = get_content();
 	convert(from, to, content);
+}
+
+function share_btn_click() {
+	var content = url_encode(get_content());
+	var url = document.location.protocol + '//' + document.location.host+ '/edit/' + content;
+	$('#share-url').val(url);
+	$('#share-url').show();
+}
+
+function url_encode(string) {
+	return encodeURIComponent(string);
+}
+
+function get_content() {
+	return $('#editor-input').val();
 }
